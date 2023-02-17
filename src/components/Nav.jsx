f2b99bb294069d4e1732d9b1217a5e48ba169b41
ferutizi <fer.utizi@gmail.com>
2023-02-17 18:05:00 -0300

    added google-map and wp messages api

---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+    return(
+        <div style={{backgroundColor: '#f5f0dd', width: '100%', height: '8em', display: 'flex', alignItems: 'center'}}>
+            <img src={require('../images/logo.png')} style={{height: '80%'}}></img>
+        </div>
+    );
+}
+
+export default Nav;
