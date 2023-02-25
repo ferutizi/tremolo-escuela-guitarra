f8dbfba97933f980b4605db762f9c852cf0a6fde
ferutizi <fer.utizi@gmail.com>
2023-02-25 17:46:53 -0300

    form, form steps, emailjs, submitOK

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,39 +1,60 @@
+import useFormulario from '../hooks/useFormulario';
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import './Formulario.scss';
 
 const Formulario = () => {
-    const active = true;
+    const [active, setActive] = useState(true);
+    const [submitted, setSubmitted] = useState(false);
+    const [formulario, handleChange, reset] = useFormulario();
+
+    const form = useRef();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_tylurvk', 'template_oy0n19c', form.current, 'BWCL7ZUqJm6x267aG')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        setSubmitted(true);
+        reset();
+        setSubmitted(false);
+    }
+    console.log(active)
     return(
         <div className="inscription--container">
-            <form>
-                <div className={`inscription--form form--${active}`}>
-                    <input placeholder='Nombre' required className="inscription--input" />
-                    <input placeholder='Apellido' required className="inscription--input" />
-                    <input placeholder='Correo Electrónico' required className="inscription--input" />
-                    <input placeholder='Teléfono' required className="inscription--input" />
-                    <select className='inscription--input' required defaultValue={false} style={{color: '#777'}}>
+            <form onSubmit={handleSubmit} ref={form} style={{display: 'flex'}}>
+                <div className={`inscription--form form--${active} form--first`}>
+                    <input name="name" value={formulario.value} onChange={handleChange} autoFocus type="text" placeholder='Nombre' required className="inscription--input" />
+                    <input name="surname" value={formulario.value} onChange={handleChange} type="text" placeholder='Apellido' required className="inscription--input" />
+                    <input name="email" value={formulario.value} onChange={handleChange} type="text" placeholder='Correo Electrónico' required className="inscription--input" />
+                    <input name="phone" value={formulario.value} onChange={handleChange} type="number" placeholder='Teléfono' required className="inscription--input" />
+                    <select name="age-group" onChange={handleChange} className='inscription--input' required defaultValue={false} style={{color: '#777'}}>
                         <option hidden label='Grupo Etario' />
                         <option value='Niño'>Niño</option>
                         <option value='Adolescente'>Adolescente</option>
                         <option value='Adulto'>Adulto</option>
                     </select>
-                    <button className='inscription--button' type='button'>Enviar</button>
+                    <button onClick={() => setActive(!active)} className='inscription--button' type='button'>Siguiente</button>
                 </div>
-                <div className={`inscription--form form--${active}`}>
-                    <select className='inscription--input' required defaultValue={false} style={{color: '#777'}}>
+                <div className={`inscription--form form--${!active} form--second`}>
+                    <select name="city" onChange={handleChange} className='inscription--input' required defaultValue={false} style={{color: '#777'}}>
                         <option hidden label='Ciudad' />
                         <option value='Bahía Blanca'>Bahía Blanca</option>
                         <option value='Punta Alta'>Punta Alta</option>
                     </select>
-                    <select className='inscription--input' required defaultValue={false} style={{color: '#777'}}>
+                    <select name="professor" onChange={handleChange} className='inscription--input' required defaultValue={false} style={{color: '#777'}}>
                         <option hidden label='Profesor de preferencia' />
                         <option value='sin preferencia'>Sin preferencia</option>
                         <option value='Uziel Leonel Acuña Martínez'>Uziel Leonel Acuña Martínez</option>
                         <option value='Fernando Utizi'>Fernando Utizi</option>
                     </select>
-                    <select className='inscription--input' required defaultValue={false} style={{color: '#777'}}>
+                    <select name="hour" onChange={handleChange} className='inscription--input' required defaultValue={false} style={{color: '#777'}}>
                         <option hidden label='Horario' />
                         <option value='Lunes 17'>Lunes 17</option>
                     </select>
+                    <button type="submit" className="inscription--button">Enviar Inscripción</button>
                 </div>
             </form>
         </div>
