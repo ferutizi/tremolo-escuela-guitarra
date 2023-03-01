d4a4f8a67f25fe58237e88980a7e4a7cc6b3b08c
ferutizi <fer.utizi@gmail.com>
2023-03-01 16:45:30 -0300

    button in aboutUs: fixed hover, fixed Link hover

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -17,9 +17,11 @@ const AboutUs = () => {
                     <p className='description--text'>Somos una escuela itinerante de guitarra que funciona en Punta Alta y Bahía Blanca, está abierta a todo aquel que quiera aprender este instrumento y disfrutar de la música, cualquiera sea su nivel, a partir de los 8 años
                         hasta adultos mayores. Tenemos como objetivo llevar la enseñanza musical a diferentes espacios culturales y sociedades de fomento
                     </p>
-                    <Link to='/inscription'>
-                        <button className='about--button'>¡Inscribete ahora!</button>
-                    </Link>
+                        <button className='about--button'>
+                            <Link to='/inscription' style={{textDecoration: 'none', color: 'inherit'}}>
+                                ¡Inscribete ahora!
+                            </Link>
+                        </button>
                 </div>
             </div>
         </section>
