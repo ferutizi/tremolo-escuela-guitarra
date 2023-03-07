3e0370f96419861aace7fae7e025ce8dff117248
ferutizi <fer.utizi@gmail.com>
2023-03-07 12:38:45 -0300

    whatsapp number

--- src/components/WhatsappIcon.jsx
+++ src/components/WhatsappIcon.jsx
@@ -2,7 +2,7 @@ import './WhatsappIcon.scss';
 
 const WhatsappIcon = () => {
     return(
-        <a href="https://api.whatsapp.com/send?phone=2932543010&text=Hola, me quiero inscribir a clases de guitarra" target="_blank" >
+        <a href="https://api.whatsapp.com/send?phone=+5492932543010&text=Hola, me quiero inscribir a clases de guitarra" target="_blank" >
             <img src={require('../images/wp_c.png')} alt="whatsapp-contact" className="icon--wp" />
         </a>
     );
